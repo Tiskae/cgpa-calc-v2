11761288519b07b3e25dc60c67eb80122d5ad265
Tiskae <macbook@Tiskaes-MacBook-Pro.local>
2024-04-17 18:45:23 +0100

    CGPA calculator basic func

--- src/app/cgpa-calc/page.tsx
+++ src/app/cgpa-calc/page.tsx
@@ -5,6 +5,7 @@ import Styles from "./cgpa-calc.module.scss";
 import { useState } from "react";
 import Progress from "react-circle-progress-bar";
 import FormField from "@/components/FormField/FormField";
+import clsx from "clsx";
 
 interface CGPAdataType {
   lastCGPA: string;
@@ -18,18 +19,20 @@ function CgpaCalculatorPage() {
     lastCGPA: "3.7", completedSemesters: "2", currentGPA: "5.0"
   })
   const [CGPA, setCGPA] = useState<number>(0);
+  const [showResultPage, setShowResultPage] = useState<boolean>(false);
 
   const calculateCGPA = (): void => {
     let CGPA = ((+CGPAdata.lastCGPA * +CGPAdata.completedSemesters) + +CGPAdata.currentGPA) / (+CGPAdata.completedSemesters + 1);
     setCGPA(CGPA);
+    setShowResultPage(true);
   }
 
 
   return (
     <main className={Styles.page}>
       <div className={Styles.container}>
+        <form className={clsx([Styles.cgpa__form, showResultPage && Styles.hidden])}>
         <h1 className={Styles.heading}>Calculate Your CGPA</h1>
-        <form className={Styles.cgpa__form}>
           <FormField id="last-cgpa" label="Your last CGPA" type="number" value={CGPAdata.lastCGPA} onChange={
              e => {
               setCGPAdata(oldData => {
@@ -43,8 +46,8 @@ function CgpaCalculatorPage() {
           <FormField id="completed-semesters" label="No of completed semesters" type="number" value={CGPAdata.completedSemesters} onChange={
              e => {
               setCGPAdata(oldData => {
-                const {completedSemesters, currentGPA} = oldData;
-                const updatedData:CGPAdataType = {lastCGPA: e.target.value, completedSemesters, currentGPA};
+                const {lastCGPA, currentGPA} = oldData;
+                const updatedData:CGPAdataType = {lastCGPA, completedSemesters: e.target.value, currentGPA};
                 return updatedData
               })
             }
@@ -59,14 +62,16 @@ function CgpaCalculatorPage() {
                 })
               }
           }/>
-
-          <button className="btn" onClick={e => {
-            e.preventDefault();
-            calculateCGPA();
-          }}>Calculate CGPA</button>
+          <div className={Styles.action_btns}>
+            <Link href="/" className="btn">Back to menu</Link>
+            <button className="btn" onClick={e => {
+              e.preventDefault();
+              calculateCGPA();
+            }}>Calculate CGPA</button>
+          </div>
         </form>
 
-        <div>
+        <div className={clsx([Styles.result, showResultPage && Styles.visible])}>
           <h3>CGPA: {CGPA}</h3>
           <Progress
               progress={(CGPA / 5) * 100}
@@ -78,9 +83,8 @@ function CgpaCalculatorPage() {
               className={Styles.circle_progress}
               strokeWidth={7}
             />
+          <button className="btn" onClick={()=> setShowResultPage(false)}>Go back</button>
         </div>
-
-        <Link href="/" className="btn">Back to menu</Link>
       </div>
     </main>
   );

8a4616aec68d34388230fa5bd90dcf6a28183731
Tiskae <macbook@Tiskaes-MacBook-Pro.local>
2024-04-29 08:57:49 +0100

    basic Settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,8 +2,17 @@ import Styles from "./settings.module.scss";
 
 function SettingsPage() {
   return (
-    <main>
-      <h2>Settings</h2>
+    <main className={Styles.page}>
+      <div className={Styles.container}>
+        <h2 className={Styles.heading}>Settings</h2>
+        <ul>
+          <li>CGPA Scale: 4 / 5 / 7</li>
+          <li>Save your data for next time: True / False</li>
+          <li>Theme: Light / Dark / System</li>
+          <li>Analytics + Cookies Consent: Accept / Reject</li>
+          <li>Report Issue</li>
+        </ul>
+      </div>
     </main>
   );
 }
